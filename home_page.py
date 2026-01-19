e63a969a83f5bd6cc57d3e412e31b17a2dba4052
Goats <2005ayatarek@gmail.com>
2026-01-19 08:12:13 +0200

    home page edits and comment

--- home_page.py
+++ home_page.py
@@ -1,56 +1,65 @@
-from PyQt6.QtWidgets import (QApplication, QMainWindow, QWidget, 
+from PyQt6.QtWidgets import (QWidget, 
                              QPushButton, QLabel, QGraphicsScene, 
                              QGraphicsView, QGraphicsPixmapItem, 
                              QVBoxLayout, QFrame, QHBoxLayout, 
                              QSizePolicy, QDialog, QGridLayout, 
-                             QLineEdit, QCheckBox, QScrollArea,
-                             QLayout)
-from PyQt6.QtCore import Qt, pyqtSignal
+                             QCheckBox, QScrollArea,QLayout)
+from PyQt6.QtCore import Qt, pyqtSignal, QPropertyAnimation, QEasingCurve, QRectF
 from PyQt6.QtGui import QPixmap
 from store_utils import default_theme
-import sys
 import os
 
-class RoomScene(QWidget):
 
+### MAIN SCENE ###
+class RoomScene(QWidget):
+    '''main container for viewing game, has camera and side and bottom panels'''
+
+    #signals for navigation
     logout_signal = pyqtSignal()
     request_clothing_store = pyqtSignal()
     request_furniture_store = pyqtSignal()
     request_task_entry = pyqtSignal()
-    request_task_status_update = pyqtSignal(int, int)
+    request_task_status_update = pyqtSignal(int, int) 
     request_subtask_status_update = pyqtSignal(object, int, int, int)
     request_task_removal = pyqtSignal(int)
 
     def __init__(self, game_data):
+        '''builds cameraview and panels
+        input: object'''
         super().__init__()
         self.game_data = game_data
         self.setMinimumSize(1280, 720)
         self.styles = default_theme
 
+        #main layout (center area and side panel)
         self.main_layout = QHBoxLayout(self)
         self.main_layout.setContentsMargins(0, 0, 0, 0)
         self.main_layout.setSpacing(0)
 
+        #center container(camera bottom panel)
         self.center_container = QWidget()
         self.center_layout = QVBoxLayout(self.center_container)
         self.center_layout.setContentsMargins(0, 0, 0, 0)
         self.center_layout.setSpacing(0)
 
+        #where items r placed
         self.scene = QGraphicsScene()
-        self.scene.setSceneRect(175, -40, 1080, 520)
+        self.scene.setSceneRect(100, -40, 1080, 520)
 
-        # The Camera View
         self.camera = Camera(self.scene, self.styles)
         self.camera.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
         self.camera.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)   
         
+        #buttons that toggle side and bottom panel
         self.camera.bottom_btn.clicked.connect(self.toggle_bottom)
         self.camera.side_btn.clicked.connect(self.toggle_side)
 
         self.setup_bottom_panel()
+        # Widgets to center layout
         self.center_layout.addWidget(self.camera)
         self.center_layout.addWidget(self.bottom_panel)
         self.setup_side_panel()
+        #main section to horizontal lauot
         self.main_layout.addWidget(self.center_container)
         self.main_layout.addWidget(self.side_panel)
 
@@ -58,14 +67,16 @@ class RoomScene(QWidget):
         self.refresh_view(self.game_data)
 
     def update_game_data(self, data):
+        ''' connects game data
+        input: object'''
         self.game_data = data
 
     def setup_side_panel(self):
-        '''Create Side Panel'''
+        '''makes expandable size panel that has the task list'''
         self.side_panel = QFrame()
         self.side_panel.setFixedWidth(350)
         self.side_panel.setStyleSheet(self.styles.panel_style())
-       
+        
         layout = QVBoxLayout(self.side_panel)
         layout.setSpacing(10)
         layout.setContentsMargins(10, 15, 10, 15)
@@ -78,6 +89,7 @@ class RoomScene(QWidget):
         self.scroll_area = QScrollArea()
         self.scroll_area.setWidgetResizable(True)
         self.scroll_area.setStyleSheet(self.styles.scrollbar_style())
+        self.scroll_area.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
 
         self.card_container = QWidget()
         self.card_container.setStyleSheet("background: transparent;")
@@ -98,9 +110,9 @@ class RoomScene(QWidget):
         layout.addWidget(task_entry_nav, 0, Qt.AlignmentFlag.AlignHCenter)        
     
     def setup_bottom_panel(self):
-        '''Create Bottom Panel'''
+        '''makes bottom panel with the nav buttons'''
         self.bottom_panel = QFrame()
-        self.bottom_panel.setFixedHeight(70)
+        self.bottom_panel.setMaximumHeight(70)
         self.bottom_panel.setStyleSheet(self.styles.panel_style())
 
         layout = QHBoxLayout(self.bottom_panel)
@@ -123,7 +135,7 @@ class RoomScene(QWidget):
         layout.addWidget(btn2)
     
     def setup_settings(self):
-        '''Create settings pop-up'''
+        '''opens the settings panel QDialog with logout button'''
         settings = QDialog(self)
         settings.setWindowTitle('System Settings')
         settings.setFixedSize(300, 200)
@@ -147,27 +159,71 @@ class RoomScene(QWidget):
             self.logout()
     
     def toggle_bottom(self):
-        '''Toggle bottom panel visability'''
-        if self.bottom_panel.isVisible():
-            self.bottom_panel.hide()
-        else:
-            self.bottom_panel.show()
+        '''Animate bottom panel'''
+        cur = self.bottom_panel.maximumHeight()
+        target = 70 if cur == 0 else 0
+        
+        self.anim_bot = QPropertyAnimation(self.bottom_panel, b"maximumHeight")
+        self.anim_bot.setDuration(500)
+        self.anim_bot.setStartValue(cur)
+        self.anim_bot.setEndValue(target)
+        self.anim_bot.setEasingCurve(QEasingCurve.Type.OutQuint)
+        self.anim_bot.start()
     
     def toggle_side(self):
-        '''Toggle bottom panel visability'''
-        if self.side_panel.isVisible():
-            self.side_panel.hide()
-            self.scene.setSceneRect(100, -40, 1080, 520)
-        else:
-            self.side_panel.show()
-            self.scene.setSceneRect(175, -40, 1080, 520)
+        '''animate sdie panel and adjusts the camera center'''
+        cur = self.side_panel.width()
+        target = 350 if cur == 0 else 0
+        
+        self.scroll_area.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        
+        self.anim1 = QPropertyAnimation(self.side_panel, b"minimumWidth")
+        self.anim2 = QPropertyAnimation(self.side_panel, b"maximumWidth")
+        
+        for a in (self.anim1, self.anim2):
+            a.setDuration(450)
+            a.setStartValue(cur)
+            a.setEndValue(target)
+            a.setEasingCurve(QEasingCurve.Type.OutQuint)
+            a.finished.connect(lambda: self.on_side_anim_finished(target))
+            a.start()
+        
+        # link anim to camera 
+        # prevents it from jumping to final positon by recalc centerpoint per frame
+        self.anim1.valueChanged.connect(lambda: self.recenter_camera()) 
+
+    def on_side_anim_finished(self, target):
+        '''enable scrollbar when anim finsihes'''
+        if target > 0:
+            self.scroll_area.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAsNeeded)
+        self.recenter_camera()
 
     def resizeEvent(self, event):
-        '''Adjust button postions upon resize'''
+        '''Handles the positioning when window is resized'''
         super().resizeEvent(event)
         self.update_button_positions()
+        self.recenter_camera()
+
+    def recenter_camera(self):
+        '''calc offset to center room based on the sidepanel width'''
+        if not self.scene.items():
+            return
+            
+        items_rect = self.scene.itemsBoundingRect()
+        center_point = items_rect.center()
+
+        current_width = self.side_panel.width()
+        if current_width > 0:
+            ratio = current_width / 350.0
+            offset = 5 * ratio 
+            
+            center_point.setX(center_point.x() + offset)
+
+        self.camera.centerOn(center_point)
 
     def update_task_panel(self, user_task_list):
+        '''calears and remakes the side panel
+        input:list of object'''
         while self.card_container_layout.count():
             item = self.card_container_layout.takeAt(0)
             widget = item.widget()
@@ -177,8 +233,10 @@ class RoomScene(QWidget):
         card_list = []
         for i in range(len(user_task_list)):
             if user_task_list[i].subdivisions != 0:
+                #create a card with the subtasks
                 card = UserDivTaskCard(user_task_list[i])
                 card.update_task_status_database.connect(self.request_task_status_update.emit)
+                #cards emit signal to room scen which roomscene emits to main to do logic
                 card.update_subtask_status_database.connect(
                     lambda status, subtask_id, taskid, c=card: 
                     self.request_subtask_status_update.emit(c, status, subtask_id, taskid)
@@ -186,6 +244,7 @@ class RoomScene(QWidget):
                 card.delete_request.connect(self.request_task_removal.emit)
                 card_list.append(card)
             else:
+                #create a card without subtask
                 card = UserTaskCard(user_task_list[i])
                 card.update_task_status_database.connect(self.request_task_status_update.emit)
                 card.delete_request.connect(self.request_task_removal.emit)
@@ -195,10 +254,11 @@ class RoomScene(QWidget):
             self.card_container_layout.addWidget(card)
 
     def update_divtask_label(self, card, status):
+        '''update the strike through on the task headers'''
         card.update_center_label(status)
     
     def update_button_positions(self):
-        '''Adjust button postions'''
+        '''position overlay butons on camera view'''
         y_bot = self.camera.height() - self.camera.bottom_btn.height()
         x_bot = (self.camera.width() - self.camera.bottom_btn.width()) // 2
         self.camera.bottom_btn.move(x_bot, y_bot - 5) 
@@ -212,19 +272,22 @@ class RoomScene(QWidget):
         self.camera.money_indicator.move(money_x, margin)
 
     def logout(self):
-        '''Signal logout for page change'''
+        '''emits logout sginal'''
         self.logout_signal.emit()
 
     def refresh_view(self, data):
+        '''Refereshes money, data and reloads the furniture
+        input: object'''
         self.camera.update_money(self.game_data.money)
         self.update_game_data(data)
         self.scene.clear()
         self.load_furniture()
-
-        center = self.scene.sceneRect().center()
-        self.camera.centerOn(center.x()-800, center.y())
+        self.recenter_camera()
 
     def get_image_path(self, item_name):
+        '''Find all images assets based on name
+        input: str
+        output: list of str'''
         assets_folder = 'assets'
         paths = []
         if not os.path.exists(assets_folder): return []
@@ -236,6 +299,7 @@ class RoomScene(QWidget):
         return paths
     
     def load_furniture(self):
+        '''add furniture images to scene based on pllaced furniture data'''
         scale_factor = 0.8
 
         for item in self.game_data.placed_furniture:
@@ -263,11 +327,12 @@ class RoomScene(QWidget):
                 pix_item.setZValue(item.get('z', 0))
                 self.scene.addItem(pix_item)
 
-                
-
 class Camera(QGraphicsView):
+    '''A custom QgraphicsView that acts as camera for scene'''
     req_settings = pyqtSignal()
     def __init__(self, scene, styles):
+        '''init view and overla buttons
+        input:QGraphicsene, object'''
         super().__init__(scene)
         self.styles = styles
         self.margin = 10
@@ -299,18 +364,19 @@ class Camera(QGraphicsView):
         self.money_indicator.move(self.settings_btn.width()+self.margin, self.margin)
 
     def update_money(self, amount):
-        '''Update money display'''
+        '''Update the visual money disp
+        input: int'''
         self.money_indicator.setText(f'${amount}')
         self.money_indicator.adjustSize()
         self.update_button_positions()
-        
 
     def resizeEvent(self, event):
-        '''Adjust button postions upon resize'''
+        '''Update overlay button position when resize'''
         super().resizeEvent(event)
         self.update_button_positions()
 
     def update_button_positions(self):
+       '''recalc new coords for buttons absed on window size'''
        margin = 10
        y_bot = self.height() - self.bottom_btn.height()
        self.bottom_btn.move((self.width() - self.bottom_btn.width()) // 2, y_bot)
@@ -321,20 +387,25 @@ class Camera(QGraphicsView):
        self.money_indicator.move(money_x, margin) 
     
     def mousePressEvent(self, event):
-        '''make it a pointer not hand'''
+        '''override mousepress to keep curso consisten'''
         super().mousePressEvent(event)
         self.viewport().setCursor(Qt.CursorShape.ArrowCursor)
 
     def mouseReleaseEvent(self, event):
-        '''make it arrow not hand'''
+        '''override mousepress to keep curso consisten'''
         super().mouseReleaseEvent(event)
         self.viewport().setCursor(Qt.CursorShape.ArrowCursor)
 
+### TASK CARDS ###
+
 class UserTaskCard(QFrame):
+    ''' taks card widget for side bar'''
     update_task_status_database = pyqtSignal(int, int)
     delete_request = pyqtSignal(int)
 
     def __init__(self, user_task):
+        '''constructs task card UI
+        input:object'''
         super().__init__()
         self.setObjectName("Card") 
         self.setFixedSize(305, 65) 
@@ -387,17 +458,22 @@ class UserTaskCard(QFrame):
         layout.setColumnStretch(2, 0)
 
     def task_status_updated(self, checked):
+        '''emit signal to ubdate dtabase and strike out text
+        input: bool'''
         self.update_task_status_database.emit(int(checked), self.taskid)
         font = self.checkbox.font()
         font.setStrikeOut(checked)
         self.checkbox.setFont(font)
 
 class UserDivTaskCard(QFrame):
+    '''task card widget with subtasks'''
     update_task_status_database = pyqtSignal(int, int)
     update_subtask_status_database = pyqtSignal(int, int, int)
     delete_request = pyqtSignal(int)
 
     def __init__(self, user_task):
+        '''make task card UI
+        input: object'''
         super().__init__()
         self.setObjectName("Card") 
         self.setFixedWidth(305)
@@ -411,6 +487,7 @@ class UserDivTaskCard(QFrame):
         layout = QVBoxLayout(self)
         layout.setSpacing(0)
         layout.setContentsMargins(0, 0, 0, 0)
+        #fix layout to fit around children to make it shrink back down ithout space in scroll area
         layout.setSizeConstraint(QLayout.SizeConstraint.SetFixedSize)
 
         main_card_container = QWidget()
@@ -483,6 +560,8 @@ class UserDivTaskCard(QFrame):
         layout.addStretch()
 
     def add_checkbox_list(self, subtask_list):
+        '''Iterates thru subtask list and makes checkboxes
+        input: list of dict'''
         for subtask in subtask_list:
             checkbox = QCheckBox(f"{subtask['name']}")
             checkbox.setCursor(Qt.CursorShape.PointingHandCursor)
@@ -490,6 +569,7 @@ class UserDivTaskCard(QFrame):
             font = checkbox.font()
             font.setStrikeOut(checkbox.isChecked())
             checkbox.setFont(font)
+            #we have ti do subtask id = subtask otherwise it will take the value when the button is clicked
             checkbox.toggled.connect(
                 lambda checked, subtask_id=subtask['subtask_id'], chk = checkbox: 
                 self.subtask_status_updated(checked, subtask_id, chk)
@@ -498,17 +578,20 @@ class UserDivTaskCard(QFrame):
         self.subtasks_container_layout.addStretch()
 
     def subtask_status_updated(self, checked, subtask_id, checkbox):
+        '''emit signal for subtask to strike through the text'''
         self.update_subtask_status_database.emit(int(checked), subtask_id, self.taskid)
         font = checkbox.font()
         font.setStrikeOut(checked)
         checkbox.setFont(font)
 
     def update_center_label(self, status):
+        '''Update main subtask if all task are complete'''
         font = self.center_label.font()
         font.setStrikeOut(bool(status))
         self.center_label.setFont(font)
 
     def toggle_subtask_container(self):
+        '''expand, collapse subtask list view'''
         if self.subtasks_container.isVisible():
             self.menu_btn.setText('►')
             self.subtasks_container.hide()
